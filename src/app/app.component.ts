251709dfc366b917f7e74469f07c4660c9b4d160
Gaelle Walczyna <gaellewalczyna@gmail.com>
2023-09-17 21:17:30 +0200

    ajout de directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,33 +7,41 @@ import { FaceSnap } from './models/face-snap.models';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+
+  faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(    
-      'Le cocker',
-      'Le plus gentil chien du monde !',
-      'https://unpointculture.com/wp-content/uploads/2023/04/race-chien-cocker-anglais-1220x600.jpg',
-      new Date(),
-      0
-    )
-
-    this.myOtherSnap = new FaceSnap (
-      'Le panda roux',
-      'Le plus beau',
-      'https://img.freepik.com/photos-gratuite/beau-panda-rouge-voie-disparition-arbre-vert_475641-1326.jpg?size=626&ext=jpg&ga=GA1.1.1869783212.1694793886&semt=ais',
-      new Date(),
-      0
-    )
-
-    this.myLastSnap = new FaceSnap (
-      'Le manchot',
-      'Mon préféré',
-      'https://i.pinimg.com/236x/70/3b/94/703b94e86a5dc670735b687551bb4733--penguin-bird-penguin-facts.jpg',
-      new Date(),
-      0
-    )
+    this.faceSnaps = [
+      {    
+        title:'Le cocker',
+        description: 'Le plus gentil chien du monde !',
+        imageUrl: 'https://unpointculture.com/wp-content/uploads/2023/04/race-chien-cocker-anglais-1220x600.jpg',
+        createdDate: new Date(),
+        snaps: 60,
+        location: 'Paris'
+      },
+      {
+        title: 'Le panda roux',
+        description: 'Le plus beau',
+        imageUrl: 'https://parc.branfere.com/wp-content/uploads/sites/2/2018/07/panda-roux-carnivores-branfere.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'Bali'
+      },
+      {
+        title: 'Le manchot',
+        description: 'Mon préféré',
+        imageUrl: 'https://t4.ftcdn.net/jpg/00/01/83/73/240_F_1837302_bzSusKQ2holEpeb4NU5trJvkYnlZpd.jpg',
+        createdDate: new Date(),
+        snaps: 0
+      },
+      {    
+        title:'La Nouvelle Zélande',
+        description: 'Le plus beau pays du monde',
+        imageUrl: 'https://static.asia.fr/cxfile/photosi/diapo-nouvelle-zelande-1-go.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+      }
+    ]
   };
 }
--- src/app/models/face-snap.models.ts
+++ src/app/models/face-snap.models.ts
@@ -18,11 +18,21 @@
 // TOUT CA peut être écrit directement comme ça : 
 // En fait, je crée un nouveau type
 export class FaceSnap{
-    constructor(
-        public title: string, 
-        public description: string, 
-        public imageUrl: string, 
-        public createdDate: Date, 
-        public snaps: number) {
-        }
+    // constructor(
+    //     public title: string, 
+    //     public description: string, 
+    //     public imageUrl: string, 
+    //     public createdDate: Date, 
+    //     public snaps: number,
+    //     // je mets un ? pour dire que cette propriété est optionnelle
+    //     public loccation?: string) {
+    //     }
+
+    // Je rends plus facile à lire en enlevant le constructor :
+    title!: string;
+    description!: string;
+    imageUrl!: string;
+    createdDate!: Date;
+    snaps!: number;
+    location?: string;
 }
